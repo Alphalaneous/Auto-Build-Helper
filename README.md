c23edc1e6e14fe9979288d8f8d13c99151280d25
Alphalaneous <38200084+Alphalaneous@users.noreply.github.com>
2024-09-22 03:10:38 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Auto Build Helper
+
+Automatically run build helper when placing custom objects and pasting.
+
+Build Helper icon by Brift
+
+## Known Issues
+
+Swipe placing very quickly may randomly cause groups to be improperly build helpered, this is likely a GD issue cannot be fixed easily within a mod.
---
+++ src/main.cpp
@@ -0,0 +1,81 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/EditorUI.hpp>
+#include <Geode/modify/LevelEditorLayer.hpp>
+
+using namespace geode::prelude;
+
+class $modify(MyEditorUI, EditorUI) {
+
+	struct Fields {
+		Ref<EditorPauseLayer> m_pauseLayer;
+		bool m_autoBuildHelperEnabled = false;
+	};
+
+	bool init(LevelEditorLayer* editorLayer) {
+
+		if (!EditorUI::init(editorLayer)) return false;
+
+		m_fields->m_pauseLayer = EditorPauseLayer::create(editorLayer);
+		m_fields->m_pauseLayer->setTouchEnabled(false);
+		m_fields->m_pauseLayer->setKeyboardEnabled(false);
+		m_fields->m_pauseLayer->setKeypadEnabled(false);
+
+		CCTouchDispatcher::get()->unregisterForcePrio(m_fields->m_pauseLayer);
+		CCTouchDispatcher::get()->removeDelegate(m_fields->m_pauseLayer);
+
+		queueInMainThread([this] {
+			if (auto delegate = typeinfo_cast<CCTouchDelegate*>(m_fields->m_pauseLayer.data())) {
+				if (auto handler = CCTouchDispatcher::get()->findHandler(delegate)) {
+					CCTouchDispatcher::get()->setPriority(100000, handler->getDelegate());
+				}
+			}
+		});
+
+		if (CCMenu* menu = typeinfo_cast<CCMenu*>(getChildByID("toolbar-toggles-menu"))) {
+
+			CCSprite* autoBuildHelperSpr = CCSprite::create("build_helper.png"_spr);
+			
+			ButtonSprite* autoBuildHelperSprOn = ButtonSprite::create(autoBuildHelperSpr, 40, true, 40, "GJ_button_02.png", 1);
+			ButtonSprite* autoBuildHelperSprOff = ButtonSprite::create(autoBuildHelperSpr, 40, true, 40, "GJ_button_01.png", 1);
+
+			autoBuildHelperSpr->setPositionY(autoBuildHelperSpr->getPositionY()-2);
+
+			autoBuildHelperSprOn->setContentSize({40, 40});
+			autoBuildHelperSprOff->setContentSize({40, 40});
+
+			CCMenuItemToggler* toggler = CCMenuItemToggler::create(autoBuildHelperSprOn, autoBuildHelperSprOff, this, menu_selector(MyEditorUI::onToggleAutoBuildHelper));
+			toggler->setID("build-helper-button"_spr);
+			toggler->toggle(true);
+			menu->addChild(toggler);
+			menu->updateLayout();
+		}
+
+		return true;
+	}
+
+	void onToggleAutoBuildHelper(CCObject* sender) {
+		CCMenuItemToggler* toggler = static_cast<CCMenuItemToggler*>(sender);
+		m_fields->m_autoBuildHelperEnabled = toggler->isOn();
+		 //editor sounds compat
+		sender->setTag(m_selectedMode);
+        EditorUI::toggleMode(sender);
+	}
+};
+
+class $modify(MyLevelEditorLayer, LevelEditorLayer) {
+
+    cocos2d::CCArray* createObjectsFromString(gd::string const& p0, bool p1, bool p2){
+		auto ret = LevelEditorLayer::createObjectsFromString(p0, p1, p2);
+		
+		if (m_editorUI) {
+			MyEditorUI* mui = static_cast<MyEditorUI*>(m_editorUI);
+			if (!p1 && !p2 && mui->m_fields->m_autoBuildHelperEnabled){
+				queueInMainThread([mui] {
+					mui->m_fields->m_pauseLayer->onBuildHelper(mui->m_fields->m_pauseLayer);
+				});
+			}
+		}
+
+		return ret;
+	}
+};
